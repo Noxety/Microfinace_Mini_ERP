4308ef14181b8d91db0e55ef8a70794fdc01d1e5
Noxety <thura3.shine33@gmail.com>
2026-01-29 01:36:48 +0630

    Update AppLogo component: change displayed text from 'TESTING' to 'Unity Microfinance Ltd' for improved branding.

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -7,7 +7,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-8 fill-current" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">TESTING</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">Unity Microfinance Ltd</span>
             </div>
         </>
     );
